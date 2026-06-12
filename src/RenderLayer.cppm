0e3ce4cbbaea2b5720a671d120348b314dbbe295
MyJules <mudryk200@gmail.com>
2026-06-12 09:35:57 +0200

    Render layers

---
+++ src/RenderLayer.cppm
@@ -0,0 +1,100 @@
+export module RenderLayer;
+
+import std;
+import raylib;
+
+export namespace RenderLayer {
+    class Layer {
+    public:
+        Layer(int texture_width, int texture_height, float camera_width, float camera_height);
+        ~Layer();
+        Layer(const Layer&) = delete;
+        Layer& operator=(const Layer&) = delete;
+        Layer(Layer&&) = default;
+        Layer& operator=(Layer&&) = default;
+
+        void begin() const;
+        void end() const;
+
+        raylib::Rectangle fit_rect(const raylib::Rectangle& bounds) const;
+        raylib::Rectangle fit_rect_top_left(const raylib::Rectangle& bounds) const;
+
+        void draw(const raylib::Rectangle& dest_rect) const;
+        const raylib::RenderTexture2D& target() const { return m_target; }
+
+    private:
+        int m_texture_width;
+        int m_texture_height;
+        raylib::RenderTexture2D m_target;
+        raylib::Camera2D m_camera;
+    };
+
+}
+
+module :private;
+
+namespace RenderLayer {
+
+Layer::Layer(int texture_width, int texture_height, float camera_width, float camera_height):
+    m_texture_width(texture_width),
+    m_texture_height(texture_height),
+    m_target(raylib::LoadRenderTexture(texture_width, texture_height)),
+    m_camera(raylib::Camera2D{
+        .offset = {0.0f, 0.0f},
+        .target = {0.0f, 0.0f},
+        .rotation = 0.0f,
+        .zoom = std::min(static_cast<float>(texture_width) / camera_width,
+                         static_cast<float>(texture_height) / camera_height),
+    })
+{}
+
+void Layer::begin() const {
+    raylib::BeginTextureMode(m_target);
+    raylib::BeginMode2D(m_camera);
+}
+
+void Layer::end() const {
+    raylib::EndMode2D();
+    raylib::EndTextureMode();
+}
+
+raylib::Rectangle Layer::fit_rect(const raylib::Rectangle& bounds) const {
+    const auto scale = std::min(bounds.width / static_cast<float>(m_texture_width),
+                                bounds.height / static_cast<float>(m_texture_height));
+
+    return raylib::Rectangle{
+        bounds.x + (bounds.width - m_texture_width * scale) / 2.0f,
+        bounds.y + (bounds.height - m_texture_height * scale) / 2.0f,
+        m_texture_width * scale,
+        m_texture_height * scale,
+    };
+}
+
+raylib::Rectangle Layer::fit_rect_top_left(const raylib::Rectangle& bounds) const {
+    const auto scale = std::min(bounds.width / static_cast<float>(m_texture_width),
+                                bounds.height / static_cast<float>(m_texture_height));
+
+    return raylib::Rectangle{
+        bounds.x,
+        bounds.y,
+        m_texture_width * scale,
+        m_texture_height * scale,
+    };
+}
+
+void Layer::draw(const raylib::Rectangle& dest_rect) const {
+    raylib::DrawTexturePro(m_target.texture,
+                            {0, 0,
+                            static_cast<float>(m_texture_width),
+                            -static_cast<float>(m_texture_height)},
+                            dest_rect,
+                            {0, 0},
+                            0.0f,
+                            raylib::WHITE);
+}
+
+Layer::~Layer() {
+    raylib::UnloadRenderTexture(m_target);
+}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,82 @@
 import std;
 import args;
 import raylib;
+import RenderLayer;
 
 int main(int argc, const char* argv[]) {
     const auto args = args::to_span(argc, argv);
 
-    constexpr auto window_width = 960;
-    constexpr auto window_height = 540;
+    constexpr auto virtual_width = 1280;
+    constexpr auto virtual_height = 720;
 
-    raylib::SetConfigFlags(raylib::FLAG_MSAA_4X_HINT);
-    raylib::InitWindow(window_width, window_height, "raylib modules");
+    raylib::SetConfigFlags(raylib::FLAG_WINDOW_RESIZABLE); 
+    raylib::InitWindow(1280, 720, "raylib modules");
+
+    auto world_layer = RenderLayer::Layer(virtual_width, virtual_height, 1920, 1080);
+    raylib::SetTextureFilter(world_layer.target().texture, raylib::TextureFilter::TEXTURE_FILTER_POINT);
+
+    constexpr auto ui_width = 1920;
+    constexpr auto ui_height = 1080;
+
+    auto ui_layer = RenderLayer::Layer(ui_width, ui_height, 1920, 1080);
+
     raylib::SetTargetFPS(144);
 
     while (!raylib::WindowShouldClose()) {
-        raylib::BeginDrawing();
-        raylib::ClearBackground(raylib::BLACK);
+    
+    {
+        ui_layer.begin();
+        raylib::ClearBackground(raylib::BLANK);
 
-        raylib::DrawText("Hello raylib", 10, 50, 22, raylib::RAYWHITE);
-        raylib::DrawText(std::format("argc = {}", args.size()).c_str(), 10, 80, 22, raylib::RAYWHITE);
-        raylib::DrawText(std::format("argv[0] = {}", args[0]).c_str(), 10, 110, 22, raylib::RAYWHITE);
+        raylib::DrawText("UI Layer", 40, 50, 64, raylib::RAYWHITE);
+        raylib::DrawText(std::format("FPS: {}", raylib::GetFPS()).c_str(), 5, 5, 32, raylib::GREEN);
+
+        ui_layer.end();
+    }
+
+    {
+        world_layer.begin();
+        raylib::ClearBackground(raylib::BLACK);
 
         raylib::DrawRectangle(10, 150, 100, 100, raylib::RED);
         raylib::DrawCircle(200, 200, 50, raylib::GREEN);
         raylib::DrawLine(300, 150, 400, 250, raylib::BLUE);
+        raylib::DrawTriangle({500, 150}, {450, 250}, {550, 250}, raylib::YELLOW);
+        raylib::DrawEllipse(700, 200, 50, 25, raylib::PURPLE);
+        raylib::DrawRing({800, 200}, 30, 50, 0, 360, 32, raylib::ORANGE);
+        raylib::DrawRectangleRounded({10, 300, 100, 50}, 0.2f, 4, raylib::PINK);
+        raylib::DrawRectangleRoundedLines({10, 400, 100, 50}, 0.2f, 10, raylib::MAROON);
+        raylib::DrawRectangleRoundedLinesEx({10, 500, 100, 50}, 0.2f, 10, 5.0f, raylib::SKYBLUE);
+        raylib::DrawCircleSector({200, 400}, 50, 0, 270, 32, raylib::LIME);
 
-        raylib::DrawFPS(10, 10);
+        world_layer.end();
+    }
+
+    {
+        raylib::BeginDrawing();
+        raylib::ClearBackground(raylib::RAYWHITE);
+
+        const auto screen_width = raylib::GetScreenWidth();
+        const auto screen_height = raylib::GetScreenHeight();
+
+        const auto scale_x = static_cast<float>(screen_width) / virtual_width;
+        const auto scale_y = static_cast<float>(screen_height) / virtual_height;
+        const auto scale = std::min(scale_x, scale_y);
+
+        const auto offset_x = (screen_width - virtual_width * scale) / 2.0f;
+        const auto offset_y = (screen_height - virtual_height * scale) / 2.0f;
+        const raylib::Rectangle render_rect{
+            offset_x,
+            offset_y,
+            virtual_width * scale,
+            virtual_height * scale,
+        };
+
+        world_layer.draw(render_rect);
+        ui_layer.draw(ui_layer.fit_rect_top_left(render_rect));
+
         raylib::EndDrawing();
+    }
     }
 
     raylib::CloseWindow();
